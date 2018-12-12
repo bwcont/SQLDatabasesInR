ddfc6f9c8bc4d3ef7e10c8499e55fcace72e6368
E.C <35582713+bwcont@users.noreply.github.com>
2018-12-11 18:10:46 -0600

    Update PracticeDatabaseConnection.R

--- PracticeDatabaseConnection.R
+++ PracticeDatabaseConnection.R
@@ -1,8 +1,7 @@
 #load the package to let you access your db
 library(RMySQL)
 
-#connect to your database 
-#Public db from Datacamp
+#connect to your database
 connect <- dbConnect( RMySQL::MySQL(),
                       dbname = "company",
                       host = 'courses.csrrinzqubik.us-east-1.rds.amazonaws.com',
@@ -24,9 +23,14 @@ products <- dbReadTable(connect, 'products')
 sales <- dbReadTable(connect, 'sales')
 
 #Standard SQL query for retrieving data using SQL syntax 
-dbGetQuery(connect, 'SELECT * FROM employees'
-)
+dbGetQuery(connect, 'SELECT * FROM employees;')
+dbGetQuery(connect, "
+           SELECT *
+           FROM employees
+           WHERE id BETWEEN 1 AND 6
+           ")
 
+#dbGetQuery as simple SQL command syntax
 
 #Disconnect (to be nice)
 dbDisconnect(connect)
